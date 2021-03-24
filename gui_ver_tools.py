fc4c8ce35b6deb0e2301ec7d6dc1e6cbd3ea3cbd
Cuipy <53848202+PkuCuipy@users.noreply.github.com>
2021-03-24 10:57:40 +0800

    Update gui_ver_tools.py
    all function uses [0,1]

--- gui_ver_tools.py
+++ gui_ver_tools.py
@@ -8,11 +8,11 @@ def float2int(img_mat):
     return (img_mat * 255).astype('u1')
 
 def get_preview_mat(rgba_mat, bg_color):
-    alpha_mat = int2float(rgba_mat[:, :, 3:])
+    alpha_mat = rgba_mat[:, :, 3:]
     alpha_mat = np.concatenate((alpha_mat, alpha_mat, alpha_mat), axis=2)
-    new_mat = int2float(rgba_mat[:, :, :3])
+    new_mat = rgba_mat[:, :, :3]
     new_mat = alpha_mat * new_mat + (1.0 - alpha_mat) * bg_color
-    return float2int(new_mat)
+    return new_mat
 
 def get_grey_mat(img_mat, wR=0.299, wG=0.587, wB=0.114):
     return wR * img_mat[:,:,0] + wG * img_mat[:,:,1] + wB * img_mat[:,:,2]
